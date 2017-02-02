de4cc638dd0543ca6f5b30eccf30fd3fc3bfdb81
andrewmohn <amohn@umich.edu>
2017-02-02 15:08:44 -0500

    Feb 2 3:08PM

--- Yahtzee/Yahtzee/Yahtzee/Form1.cs
+++ Yahtzee/Yahtzee/Yahtzee/Form1.cs
@@ -119,14 +119,64 @@ namespace Yahtzee
             return;
         }
 
+        private void updateTotalScore()
+        {
+            int score = 0;
+            if(scoreAces.Text != " ")
+                score += int.Parse(scoreAces.Text);
+            if(scoreTwos.Text != " ")
+                score += int.Parse(scoreTwos.Text);
+            if (scoreThrees.Text != " ")
+                score += int.Parse(scoreThrees.Text);
+            if (scoreFours.Text != " ")
+                score += int.Parse(scoreFours.Text);
+            if (scoreFives.Text != " ")
+                score += int.Parse(scoreFives.Text);
+            if (scoreSixes.Text != " ")
+                score += int.Parse(scoreSixes.Text);
+            if (threeOfAKindScore.Text != " ")
+                score += int.Parse(threeOfAKindScore.Text);
+            if (fourOfAKindScore.Text != " ")
+                score += int.Parse(fourOfAKindScore.Text);
+            if (fullHouseScore.Text != " ")
+                score += int.Parse(fullHouseScore.Text);
+            if (smallStraightScore.Text != " ")
+                score += int.Parse(smallStraightScore.Text);
+            if (largeStraightScore.Text != " ")
+                score += int.Parse(largeStraightScore.Text);
+            if (YahtzeeScore.Text != " ")
+                score += int.Parse(YahtzeeScore.Text);
+            if (chanceScore.Text != " ")
+                score += int.Parse(chanceScore.Text);
+
+            finalScore.Text = score.ToString();
+            return;
+        }
+
+        private void unlockAllDice()
+        {
+            one.unKeepDie();
+            dieOne.BorderStyle = BorderStyle.None;
+            two.unKeepDie();
+            dieTwo.BorderStyle = BorderStyle.None;
+            three.unKeepDie();
+            dieThree.BorderStyle = BorderStyle.None;
+            four.unKeepDie();
+            dieFour.BorderStyle = BorderStyle.None;
+            five.unKeepDie();
+            dieFive.BorderStyle = BorderStyle.None;
+            return;
+        }
+
         private void ButtonAccept_Click(object sender, EventArgs e)
         {
             String option = saveAsTextBox.Text;
 
             int[] rolls = new int[5] { one.dieValue(), two.dieValue(), three.dieValue(), four.dieValue(), five.dieValue() };
-            int score = 0, count = 0, temp;
+            int score = 0, count = 0, temp = 0;
+            int first = 0, second = 0, count1 = 0, count2 = 0;
 
-            if(option == "Aces" && scoreAces.Text == " ")
+            if (option == "Aces" && scoreAces.Text == " ")
             {
                 //Count 1s and mark score
                 for(int i = 0; i < rolls.Length; i++)
@@ -216,18 +266,21 @@ namespace Yahtzee
                     }//If more than three matches occur
                     if(count >= 2)
                     {
-                        //There can only be one instance of three of a kind occuring
-                        //We take the initial value that it is found and multiply it
-                        //So even if a four of a kind is in the roll, it is scored as three
-                        score = rolls[i] * 3;
+                        //Score the dice
+                        for (int k = 0; k < rolls.Length; k++)
+                        {
+                            score += rolls[k];
+                        }
                         threeOfAKindScore.Text = score.ToString();
-                        i = 3; //Break the loop
+                        rollCount = 0;
+                        return;
                     }
                     //Reset count to go around again.
                     count = 0;
                 }
+                threeOfAKindScore.Text = "0";
             }
-            else if (option == "Four of a Kind")
+            else if (option == "Four of a Kind" && fourOfAKindScore.Text == " ")
             {
                 //Count the dice
                 for (int i = 0; i < 2; i++)
@@ -242,24 +295,59 @@ namespace Yahtzee
                         {
                             count++;
                         }
-                    }//If more than three matches occur
+                    }//If more than four matches occur
                     if (count >= 3)
                     {
-                        //There can only be one instance of four of a kind occuring
-                        //We take the initial value that it is found and multiply it
-                        score = rolls[i] * 4;
+                        //Score the dice
+                        for(int k = 0; k < rolls.Length; k++)
+                        {
+                            score += rolls[k];
+                        }
                         fourOfAKindScore.Text = score.ToString();
-                        i = 3; //Break the loop
+                        rollCount = 0;
+                        return;
                     }
                     //Reset count to go around again.
                     count = 0;
                 }
+                fourOfAKindScore.Text = "0";
             }
-            else if (option == "Full House")
+            else if (option == "Full House" && fullHouseScore.Text == " ")
             {
-                
+                first = rolls[1];
+
+                for(int i = 0; second == 0; i++)
+                {
+                    if (rolls[i] != first)
+                        second = rolls[i];
+                }
+
+                for(int i = 0; i < rolls.Length; i++)
+                {
+                    if(rolls[i] == first)
+                    {
+                        count1++;
+                    }
+                    else if(rolls[i] == second)
+                    {
+                        count2++;
+                    }
+                }
+
+                if(count1 == 3 && count2 == 2)
+                {
+                    fullHouseScore.Text = "25";
+                }
+                else if(count2 == 3 && count1 == 2)
+                {
+                    fullHouseScore.Text = "25";
+                }
+                else
+                {
+                    fullHouseScore.Text = "0";
+                }
             }
-            else if (option == "Small Straight")
+            else if (option == "Small Straight" && smallStraightScore.Text == " ")
             {
                 //Sort
                 for(int i = 0; i < rolls.Length-1; i++)
@@ -275,30 +363,86 @@ namespace Yahtzee
                     }
                 }
 
+                //Check if a straight exists
                 for(int i = 0, j = 0; i < rolls.Length - 1; i++)
                 {
                     if(rolls[i] + 1 == rolls[i+1])
                     {
                         j++;
+                        if (j > 2)
+                        {
+                            smallStraightScore.Text = "30";
+                            rollCount = 0;
+                            return;
+                        }
                     }
-
-                    if(j >= 3)
+                    else
                     {
-                        smallStraightScore.Text = score.ToString();
+                        j = 0;
+                    }
+                    
+                }
+
+                smallStraightScore.Text = "0";
+            }
+            else if (option == "Large Straight" && largeStraightScore.Text == " ")
+            {
+                //Sort
+                for (int i = 0; i < rolls.Length - 1; i++)
+                {
+                    for (int j = 0; j < rolls.Length; j++)
+                    {
+                        if (rolls[i] > rolls[j])
+                        {
+                            temp = rolls[i];
+                            rolls[i] = rolls[j];
+                            rolls[j] = temp;
+                        }
                     }
                 }
-            }
-            else if (option == "Large Straight")
-            {
 
-            }
-            else if (option == "Yahtzee")
-            {
+                //Check if a straight exists
+                for (int i = 0, j = 0; i < rolls.Length - 1; i++)
+                {
+                    if (rolls[i] + 1 == rolls[i + 1])
+                    {
+                        j++;
+                        if (j > 3)
+                        {
+                            largeStraightScore.Text = "40";
+                            rollCount = 0;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        j = 0;
+                    }
+                }
 
+                largeStraightScore.Text = "0";
             }
-            else if (option == "Chance")
+            else if (option == "Yahtzee" && YahtzeeScore.Text == " ")
             {
-
+                temp = rolls[0];
+                for(int i = 0; i < rolls.Length; i++)
+                {
+                    if(rolls[i] != temp)
+                    {
+                        YahtzeeScore.Text = "0";
+                        rollCount = 0;
+                        return;
+                    }
+                }
+                YahtzeeScore.Text = "50";
+            }
+            else if (option == "Chance" && chanceScore.Text == " ")
+            {
+                for(int i = 0; i < rolls.Length; i++)
+                {
+                    score += rolls[i];
+                }
+                chanceScore.Text = score.ToString();
             }
             else
             {
@@ -306,6 +450,8 @@ namespace Yahtzee
                 return;
             }
             rollCount = 0;
+            updateTotalScore();
+            unlockAllDice();
             return;
         }
     }
